04735fe07fb2383cd0e15f5946c9812e975e5ae4
wolfgang auer <wolfgangauer@macbookpro.lan>
2025-04-13 22:22:12 +0200

    Re-enable TodoList component in App

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
- 
+import React from 'react';
+// Import wieder aktivieren:
+import TodoList from './TodoList';
+import './App.css'; // Kann bleiben, auch wenn leer
+
+function App() {
+  return (
+    <div className="App">
+      {/* "Test Inhalt" entfernen und TodoList wieder einfügen: */}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App; 
